b80fc0f741eb31aa5ed40e2189120554594e01d6
ara <joarario92@gmail.com>
2021-01-10 14:41:31 +0900

    Creting

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// var postsRouter = require('./routes/posts');
 
 var app = express();
+var router = express.Router();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+// port setup
+app.set('port', process.env.PORT || 3000);
 
 app.use(logger('dev')); 
 app.use(express.json());
@@ -21,6 +25,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// app.use('/posts', postsRouter);
+
+
+
+/****************************/
+router.route('/posts/create').post(function(req, res, next) {
+  var {subject, content} = req.body;
+
+  
+})
+
+// 라우터 객체 등록
+app.use('/', router);
+/****************************/
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +60,7 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
---
+++ routes/posts.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+console.log('posts.js');
+
+router.post('/create', function(req, res, next) {
+	
+});
+
+module.exports = router;
