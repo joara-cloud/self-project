74e03c8ad5039c12646246498cfa7d79a83efff6
ara <joarario92@gmail.com>
2021-01-17 11:55:02 +0900

    studying file - app.js

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql');
+var mysql = require('mysql');
 const multer = require('multer');
-const imageSavePath = 'public/images/'; // 파일이 저장될 경로 지정
+const imageSavePath = './public/img/'; // 파일이 저장될 경로 지정
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-// var postsRouter = require('./routes/posts');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 var router = express.Router();
@@ -21,14 +22,14 @@ app.set('view engine', 'pug');
 app.set('port', process.env.PORT || 3000);
 
 app.use(logger('dev')); 
-app.use(express.json());
+app.use(express.json()); // body-parser (express.js도 빌트인 body parser를 넣었다는 점)
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/posts', postsRouter);
+app.use('/posts', postsRouter);
 
 
 
@@ -36,89 +37,92 @@ app.use('/users', usersRouter);
 /****************************/
 
 // Database 연결
-var pool = mysql.createPool({
-  connectionLimit: 10,
-  host: 'us-cdbr-east-02.cleardb.com',
-  user: 'b911dcaae2b835',
-  port: 3306,
-  password: '7363aad2',
-  database: 'heroku_e1bf3b94a251bc3',
-  debug: false
-});
+// var pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: 'us-cdbr-east-02.cleardb.com',
+//   user: 'b911dcaae2b835',
+//   port: 3306,
+//   password: '7363aad2',
+//   database: 'heroku_e1bf3b94a251bc3',
+//   debug: false
+// });
 
-  const storage = multer.diskStorage({
-    destination(req, file, callback) {
-      callback(null, imageSavePath);
-    },
-    filename(req, file, callback) {
-      callback(null, file.originalname);
-    }
-  });
+  // const storage = multer.diskStorage({
+  //   destination(req, file, callback) {
+  //     callback(null, imageSavePath);
+  //   },
+  //   filename(req, file, callback) {
+  //     callback(null, file.originalname);
+  //   },
+  //   limits: {
+  //     files: 10
+  //   }
+  // });
 
-  const upload = multer({
-    storage
-  })
-
+  // const upload = multer({
+  //   storage
+  // })
 
 // Create post
-router.route('/posts/create').post(function(req, res, next) {
-  var {subject, content} = req.body;
-
-
-  pool.getConnection(function(err, conn) {
-    if(err) {
-      console.log('err : '+ err);
-      return;
-    }
+// router.route('/posts/create').post(upload.single('uploadImage'), function(req, res, next) {
   
-    console.log('데이터베이스 연결 스레드 아이디 : '+conn.threadId);
-    var data = {
-      subject, 
-      content
-    }
-    var exec = conn.query('insert into posts set ?', data, function(err, result) {
-      conn.release();
-      console.log('실행될 sql : ' + exec.sql);
+//   var {subject, content} = req.body;
+//   console.dir(req.file);
 
-      if(err) {
-        console.log('sql 실행도중 오류 발생함.');
-        console.dir(err);
+//   pool.getConnection(function(err, conn) {
+//     if(err) {
+//       console.log('err : '+ err);
+//       return;
+//     }
+  
+//     console.log('데이터베이스 연결 스레드 아이디 : '+conn.threadId);
+//     var data = {
+//       subject, 
+//       content
+//     }
+//     var exec = conn.query('insert into posts set ?', data, function(err, result) {
+//       conn.release();
+//       console.log('실행될 sql : ' + exec.sql);
 
-        return;
-      }
+//       if(err) {
+//         console.log('sql 실행도중 오류 발생함.');
+//         console.dir(err);
 
-      res.status(200).send({msg: '성공적으로 등록되었습니다.'});
-    })
-  })  
-})
+//         return;
+//       }
+
+//       res.status(200).send({msg: '성공적으로 등록되었습니다.'});
+//     })
+//   })  
+// })
 
 // Fetch posts
-router.route('/posts').get(function(req, res, next) {
-  var {subject, content} = req.body;
+// router.route('/posts').get(function(req, res, next) {
+//   var {subject, content} = req.body;
 
-  pool.getConnection(function(err, conn) {
-    if(err) {
-      console.log('getConnection 중 오류 : ' + err);
-      return;
-    }
+//   pool.getConnection(function(err, conn) {
+//     if(err) {
+//       console.log('getConnection 중 오류 : ' + err);
+//       return;
+//     }
 
-    var exec = conn.query('select * from posts', function(err, result) {
-      conn.release();
-      console.log('실행될 sql : ' + exec.sql);
+//     var exec = conn.query('select * from posts', function(err, result) {
+//       conn.release();
+//       console.log('실행될 sql : ' + exec.sql);
 
-      if(err) {
-        console.log('Fetch posts sql 실행도중 오류 발생함.');
-        console.dir(err);
+//       if(err) {
+//         console.log('Fetch posts sql 실행도중 오류 발생함.');
+//         console.dir(err);
 
-        return;
-      }
-      res.status(200).send({
-        msg: 'post 게시물을 불러왔습니다.',
-        posts: result
-      });
-    })
-  })
-})
+//         return;
+//       }
+//       res.status(200).send({
+//         msg: 'post 게시물을 불러왔습니다.',
+//         posts: result
+//       });
+//     })
+//   })
+// })
 
 // 라우터 객체 등록
 app.use('/', router);
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,106 @@
 var express = require('express');
+var mysql = require('mysql');
+const multer = require('multer');
 var router = express.Router();
+const imageSavePath = 'public/img/'; // 파일이 저장될 경로 지정
+
 
 console.log('posts.js');
 
-router.post('/create', function(req, res, next) {
-	
+/****************************/
+
+// Database 연결
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'us-cdbr-east-02.cleardb.com',
+  user: 'b911dcaae2b835',
+  port: 3306,
+  password: '7363aad2',
+  database: 'heroku_e1bf3b94a251bc3',
+  debug: false
 });
+
+
+  const storage = multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, imageSavePath);
+    },
+    filename(req, file, callback) {
+      callback(null, file.originalname);
+    }
+  });
+
+  const upload = multer({
+    storage
+  })
+
+// Create post
+router.post('/create', upload.single('uploadImage'), function(req, res, next) {
+  
+  var {subject, content} = req.body;
+  console.dir(req.file);
+
+  pool.getConnection(function(err, conn) {
+    if(err) {
+      console.log('err : '+ err);
+      return;
+    }
+  
+    console.log('데이터베이스 연결 스레드 아이디 : '+conn.threadId);
+    var data = {
+      subject, 
+      content
+    }
+    var exec = conn.query('insert into posts set ?', data, function(err, result) {
+      conn.release();
+      console.log('실행될 sql : ' + exec.sql);
+
+      if(err) {
+        console.log('sql 실행도중 오류 발생함.');
+        console.dir(err);
+
+        return;
+      }
+
+      res.status(200).send({msg: '성공적으로 등록되었습니다.'});
+    })
+  })  
+})
+
+// Fetch posts
+router.get('/', function(req, res, next) {
+  console.log('fetch post 호출됨');
+
+	var {subject, content} = req.body;
+
+  pool.getConnection(function(err, conn) {
+    if(err) {
+      console.log('getConnection 중 오류 : ' + err);
+      return;
+    }
+
+    var exec = conn.query('select * from posts', function(err, result) {
+      conn.release();
+      console.log('실행될 sql : ' + exec.sql);
+
+      if(err) {
+        console.log('Fetch posts sql 실행도중 오류 발생함.');
+        console.dir(err);
+
+        return;
+      }
+      res.status(200).send({
+        msg: 'post 게시물을 불러왔습니다.',
+        posts: result
+      });
+    })
+  })
+})
+
+
+/****************************/
+
+
+
 
 module.exports = router;
